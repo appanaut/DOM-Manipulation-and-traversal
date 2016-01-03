38f262870e3a4b71372fa8eadff6577af2485e7e
Scott Holland <scotthollandjr@gmail.com>
2016-01-02 16:52:30 -0800

    Add js

--- js/catvdog.js
+++ js/catvdog.js
@@ -1,1 +1,34 @@
-catvdog.js
+$(document).ready(function() {
+  $("button#hello").click(function() {
+    $("ul#cat").prepend("<li>Meow!</li>");
+    $("ul#dog").prepend("<li>Ruff!</li>");
+    $("ul#cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("button#feed").click(function() {
+    $("ul#cat").prepend("<li>Purrrr.</li>");
+    $("ul#dog").prepend("<li>Groooowwwl.</li>");
+    $("ul#cat").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("button#love").click(function() {
+    $("ul#cat").prepend("<li>Hiss!</li>");
+    $("ul#dog").prepend("<li>*Lick*</li>");
+    $("ul#cat").children("li").first().click(function(){
+      $(this).remove();
+    });
+    $("ul#dog").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+});
